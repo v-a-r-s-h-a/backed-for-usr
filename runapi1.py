9057d7262e4cd1923a5d7c71930f249b8640c4ed
manash997 <102976851+manash997@users.noreply.github.com>
2022-06-07 13:50:58 +0530

    Add files via upload

---
+++ runapi1.py
@@ -0,0 +1,17 @@
+
+import os
+from flask import Flask
+from flask import jsonify
+from requests import request
+import backendmethod
+
+
+app=Flask(__name__) #Constructor which creates an instance of class Flask.
+
+@app.route('/<string:sentence_for_usr>/')
+def displayUSR(sentence_for_usr):
+    value=backendmethod.createUSR(sentence_for_usr)
+    return jsonify(value)
+
+if __name__ == '__main__':
+    app.run(debug=True)
