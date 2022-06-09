7f65cfb8db9f7347da6d1b05bf4d0b98d3c7d331
manash997 <102976851+manash997@users.noreply.github.com>
2022-06-09 13:34:17 +0530

    Update runapi1.py

--- runapi1.py
+++ runapi1.py
@@ -4,9 +4,11 @@ from flask import Flask
 from flask import jsonify
 from requests import request
 import backendmethod
+from flask_cors import CORS
 
 
 app=Flask(__name__) #Constructor which creates an instance of class Flask.
+CORS(app)
 
 @app.route('/<string:sentence_for_usr>/')
 def displayUSR(sentence_for_usr):
